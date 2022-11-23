7a90c0c96cd39c9d03e357ef4b1e05364462f847
xuyaming <xuyaming@bytedance.com>
2022-11-23 11:12:23 +0800

    feat: update version & license info

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 OS=$1
-NAME="volcengine-cli"
+
 set -ea
 
 if [ "$OS" == "" ]
 then
   OS="darwin"
 fi
+
+NAME="volcengine-cli-"$OS
 
 CGO_ENABLED=0 GOOS=$OS GOARCH=amd64 go build  -o $NAME -tags codegen
